823fdc52af1ede417dcb4446ef392e826de5a6bc
harinandhreddy0411 <harinandhreddy@gmail.com>
2026-05-17 11:53:24 +0530

    Added Notes app

---
+++ notes-app/script.js
@@ -0,0 +1,64 @@
+const noteInput = document.getElementById("noteInput");
+const addBtn = document.getElementById("addBtn");
+const notesContainer = document.getElementById("notesContainer");
+const searchInput = document.getElementById("searchInput");
+
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+function saveNotes() {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function showNotes(filteredNotes = notes) {
+  notesContainer.innerHTML = "";
+
+  filteredNotes.forEach((note) => {
+    const div = document.createElement("div");
+
+    div.classList.add("note");
+
+    div.innerHTML = `
+      <p>${note.text}</p>
+      <button class="deleteBtn">Delete</button>
+    `;
+
+    const deleteBtn = div.querySelector(".deleteBtn");
+
+    deleteBtn.addEventListener("click", () => {
+      notes = notes.filter(n => n.text !== note.text);
+
+      saveNotes();
+
+      showNotes();
+    });
+
+    notesContainer.appendChild(div);
+  });
+}
+addBtn.addEventListener("click", () => {
+  const text = noteInput.value.trim();
+
+  if (text === "") return;
+
+  notes.push({
+    text: text
+  });
+
+  saveNotes();
+
+  showNotes();
+
+  noteInput.value = "";
+});
+
+searchInput.addEventListener("input", () => {
+  const searchText = searchInput.value.toLowerCase();
+
+  const filteredNotes = notes.filter(note =>
+    note.text.toLowerCase().includes(searchText)
+  );
+
+  showNotes(filteredNotes);
+});
+
+showNotes();
